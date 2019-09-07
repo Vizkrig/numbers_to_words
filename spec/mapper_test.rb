62d195ed1704474ee04f106a7778c6d6a9fa0865
Vishnu Prasanth <mail.vishnupk@gmail.com>
2019-09-07 13:11:37 +0530

    Add tests for validating number
    The second test makes sure invalid input is detected

--- spec/mapper_test.rb
+++ spec/mapper_test.rb
@@ -2,5 +2,17 @@ require 'rspec'
 require_relative '../lib/mapper.rb'
 
 describe Mapper do
+  describe '#valid_number?' do
+    it 'returns true' do
+      number = Mapper.new('6686787825')
+      expect(number.valid_number?).to eql(true)
+    end
+  end
 
+  describe '#valid_number?' do
+    it 'returns false for number containing 1' do
+      number = Mapper.new('1686787825')
+      expect(number.valid_number?).to eql(false)
+    end
+  end
 end
