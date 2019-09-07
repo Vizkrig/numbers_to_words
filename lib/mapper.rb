29834d4e6f0fdfa40e007e022549ce6532ffbe46
Vishnu Prasanth <mail.vishnupk@gmail.com>
2019-09-07 13:44:08 +0530

    Add number to letter hash map

--- lib/mapper.rb
+++ lib/mapper.rb
@@ -3,6 +3,17 @@ class Mapper
 
   def initialize(number)
     @number = number
+
+    @number_map = {
+      '2' => ['A','B','C'],
+      '3' => ['D','E','F'],
+      '4' => ['G','H', 'I'],
+      '5' => ['J', 'K', 'L'],
+      '6' => ['M', 'N', 'O'],
+      '7' => ['P', 'Q', 'R', 'S'],
+      '8' => ['T', 'U', 'V'],
+      '9' => ['W', 'X', 'Y', 'Z']
+    }
   end
   
   def valid_number?
