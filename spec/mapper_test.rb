919fec554d3a335389ea64d870e0d01561f1500b
Vishnu Prasanth <mail.vishnupk@gmail.com>
2019-09-07 13:30:35 +0530

    Add dictionary

--- spec/mapper_test.rb
+++ spec/mapper_test.rb
@@ -2,17 +2,17 @@ require 'rspec'
 require_relative '../lib/mapper.rb'
 
 describe Mapper do
-  describe '#valid_number?' do
+  describe 'valid number' do
     it 'returns true' do
       number = Mapper.new('6686787825')
-      expect(number.valid_number?).to eql(true)
+      expect(number.valid_number?).to be true
     end
   end
 
-  describe '#valid_number?' do
+  describe 'invalid number' do
     it 'returns false for number containing 1' do
       number = Mapper.new('1686787825')
-      expect(number.valid_number?).to eql(false)
+      expect(number.valid_number?).to be false
     end
   end
 end
